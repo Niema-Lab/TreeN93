19da86b27bdb65dd8de92b0bec9afd86652badf1
Hannah <hannahmaryfisher@gmail.com>
2020-07-29 16:22:58 -0700

    cleaned up code for filling example div

--- Build/main.js
+++ Build/main.js
@@ -34,12 +34,11 @@ var introPar = document.createElement("p");
 var introParStrings = [
   "This tool is used to create Newick formatted representations of clustering trees",
   " from the TN93 pairwise distances between the leaves.<br>",
-  "The input should be in a .csv file where the first column is distance, ",
-  "and the second and third columns are the names of the leaf nodes separated by that distance.<br>",
+  "The input should be in a CSV file, and can be formatted either as a list or as a matrix.<br>",
   "The Newick file can be saved and then uploaded in the ",
   "<a href='https://moshiri-lab.github.io/TreeN93/Viz'>TreeN93/Viz</a>",
   " clustering tool.<br><br>",
-  "Choose an input file by clicking the 'Choose File' button.<br>",
+  "Choose an input file by clicking one of the 'Choose File' buttons.<br>",
   "Save the Newick tree by copying the text from this page, ",
   "or by clicking the 'Save Newick File' button.<br><br>",
   "See the example below for formatting:"
@@ -52,74 +51,53 @@ introDiv.appendChild(introPar);
 
 /*
 add things to the example div
-also, format the example div like a flexbox
+many nested divs and paragraphs
 */
 exampleDiv.style["display"] = "flex";
 exampleDiv.style["align-items"] = "center";
-//example input div
-var exampleInputDiv = document.createElement("div");
-var exampleInputLabel = document.createElement("p");
-exampleInputLabel.innerHTML += "Your Input";
-var exampleInputParList = document.createElement("p");
-var exampleInputParOr = document.createElement("p");
-var exampleInputParMatrix = document.createElement("p");
-exampleInputParList.innerHTML = "1,A,B<br>2,A,C<br>3,A,D<br>2,B,C<br>3,B,D<br>3,C,D<br>";
-exampleInputParOr.innerHTML = "OR";
-exampleInputParMatrix.innerHTML = ",A,B,C,D<br>A,0,1,2,3<br>B,1,0,2,3<br>C,2,2,0,3<br>D,3,3,3,0";
-exampleInputParList.style["padding"] = "15px";
-exampleInputParOr.style["padding"] = "15px";
-exampleInputParMatrix.style["padding"] = "15px";
+var exampleSubDivs = [];
+var labelTexts = ["Your Input", "&rArr;", "Newick String", "&rArr;", "Tree Visualization"];
+for (var i = 0; i < 5; i ++){
+  var div = document.createElement("div");
+  div.style["margin"] = "10px";
+  var p = document.createElement("p");
+  p.innerHTML = labelTexts[i];
+  div.appendChild(p);
+  if (i % 2 != 0){ //arrow
+    p.style["font-size"] = "40px";
+  }
+  exampleSubDivs[i] = div;
+  exampleDiv.appendChild(div);
+}
 var exampleInputDivInner = document.createElement("div");
 exampleInputDivInner.style["display"] = "flex";
 exampleInputDivInner.style["align-items"] = "center";
-exampleInputDivInner.style["border-style"] = "solid";
-exampleInputDivInner.style["border-color"] = "black";
-exampleInputDivInner.style["border-width"] = "1px";
-exampleInputDivInner.appendChild(exampleInputParList);
-exampleInputDivInner.appendChild(exampleInputParOr);
-exampleInputDivInner.appendChild(exampleInputParMatrix);
-exampleInputDiv.appendChild(exampleInputLabel);
-exampleInputDiv.appendChild(exampleInputDivInner);
-//arrow 1 div
-var arrow1Div = document.createElement("div");
-var arrow1Content = document.createElement("p");
-arrow1Content.innerHTML = "&rArr;";
-arrow1Content.style["font-size"] = "40px";
-arrow1Div.appendChild(arrow1Content);
-//example newick div
-var exampleNewickDiv = document.createElement("div");
+var exampleInputParText = [
+  "1,A,B<br>2,A,C<br>3,A,D<br>2,B,C<br>3,B,D<br>3,C,D<br>",
+  "OR",
+  ",A,B,C,D<br>A,0,1,2,3<br>B,1,0,2,3<br>C,2,2,0,3<br>D,3,3,3,0"
+];
+for (var i = 0; i < 3; i ++){
+  var exampleInputPar = document.createElement("p");
+  exampleInputPar.innerHTML = exampleInputParText[i];
+  exampleInputPar.style["padding"] = "15px";
+  exampleInputDivInner.appendChild(exampleInputPar);
+}
+exampleSubDivs[0].appendChild(exampleInputDivInner);
 var exampleNewickLabel = document.createElement("p");
 exampleNewickLabel.innerHTML = "Newick String";
 var exampleNewickContent = document.createElement("p");
 exampleNewickContent.innerHTML = "(((A:1,B:1)1:1,C:2)2:1,D:3)3";
 exampleNewickContent.style["padding"] = "15px";
-exampleNewickContent.style["border-style"] = "solid";
-exampleNewickContent.style["border-color"] = "black";
-exampleNewickContent.style["border-width"] = "1px";
-exampleNewickDiv.appendChild(exampleNewickLabel);
-exampleNewickDiv.appendChild(exampleNewickContent);
-//arrow 2 div
-var arrow2Div = document.createElement("div");
-var arrow2Content = document.createElement("p");
-arrow2Content.innerHTML = "&rArr;";
-arrow2Content.style["font-size"] = "40px";
-arrow2Div.appendChild(arrow2Content);
-//example svg div
-var exampleSVGDiv = document.createElement("div");
-var exampleSVGLabel = document.createElement("p");
-exampleSVGLabel.innerHTML = "Tree Visualization";
+exampleSubDivs[2].appendChild(exampleNewickContent);
 var exampleSVGContent = document.createElement("p");
 exampleSVGContent.innerHTML = '<svg width="137.2" height="140" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><defs xmlns="http://www.w3.org/1999/xhtml"><style type="text/css"><![CDATA[.tree-selection-brush .node circle, .node ellipse, .node rect {fill: steelblue; stroke: black; stroke-width: 0.5px}.internal-node circle, .internal-node ellipse, .internal-node rect{fill: #CCC; stroke: black; stroke-width: 0.5px;}.node {font: 10px sans-serif;}.branch {fill: none; stroke: #999; stroke-width: 2px;}]]></style></defs><defs/><g transform="translate (22,0) "><g><rect x="0" width="115.2" y="0" height="140" style="visibility: hidden; "/><rect x="0" width="0" y="0" height="0"/><g transform="translate(0,0)" ><rect y="-3" width="0" height="6" style="visibility: hidden;"/></g><g transform="translate(0,0)" ><rect x="-3" width="6" height="0" style="visibility: hidden;"/></g><g transform="translate(0,0)" ><rect y="-3" width="0" height="6" style="visibility: hidden;"/></g><g transform="translate(0,0)"><rect x="-3" width="6" height="0" style="visibility: hidden;"/></g><g transform="translate(0,0)" style="display: none;"><rect x="-3" y="-3" width="6" height="6" /></g><g transform="translate(0,0)" ><rect x="-3" y="-3" width="6" height="6" style="visibility: hidden;"/></g><g transform="translate(0,0)" ><rect x="-3" y="-3" width="6" height="6" style="visibility: hidden;"/></g><g transform="translate(0,0)" style=" display: none;"><rect x="-3" y="-3" width="6" height="6" style="visibility: hidden;"/></g></g><path class="branch" d="M28.666666666666664,35V52.5H0" style="stroke: black;"><title>Length = 1</title></path><path class="branch" d="M28.666666666666664,35V17.5H0" style="stroke: black;"><title>Length = 1</title></path><path class="branch" d="M57.33333333333333,61.25V87.5H0" style="stroke: black;"><title>Length = 2</title></path><path class="branch" d="M57.33333333333333,61.25V35H28.666666666666664" style="stroke: black;"><title>Length = 1</title></path><path class="branch" d="M86,91.875V122.5H0" style="stroke: black;"><title>Length = 3</title></path><path class="branch" d="M86,91.875V61.25H57.33333333333333" style="stroke: black;"><title>Length = 1</title></path><g class="internal-node" transform="translate(86,91.875)"><circle r="3"/></g><g class="internal-node" transform="translate(57.33333206176758,61.25)"><circle r="3"/></g><g class="internal-node" transform="translate(28.66666603088379,35)"><circle r="3"/></g><g class="node" style="fill: black;" transform="translate(0,17.5)"><text dy="3.96" dx="3.96" transform="translate(-20,0)" text-anchor="start" style="opacity: 1; font-size: 12px;">A</text><line x1="0" x2="0" y1="0" y2="0" class="branch-tracer" transform="" style="opacity: 1;"/></g><g class="node" style="fill: black;" transform="translate(0,52.5)"><text dy="3.96" dx="3.96" transform="translate(-20,0)" text-anchor="start" style="opacity: 1; font-size: 12px;">B</text><line x1="0" x2="0" y1="0" y2="0" class="branch-tracer" transform="" style="opacity: 1;"/></g><g class="node" style="fill: black;" transform="translate(0,87.5)"><text dy="3.96" dx="3.96" transform="translate(-20,0)" text-anchor="start" style="opacity: 1; font-size: 12px;">C</text><line x1="0" x2="0" y1="0" y2="0" class="branch-tracer" transform="" style="opacity: 1;"/></g><g class="node" style="fill: black;" transform="translate(0,122.5)"><text dy="3.96" dx="3.96" transform="translate(-20,0)" text-anchor="start" style="opacity: 1; font-size: 12px;">D</text><line x1="0" x2="0" y1="0" y2="0" class="branch-tracer" transform="" style="opacity: 1;"/></g></g></svg>';
 exampleSVGContent.style["padding"] = "15px";
-exampleSVGContent.style["border-style"] = "solid";
-exampleSVGContent.style["border-color"] = "black";
-exampleSVGContent.style["border-width"] = "1px";
-exampleSVGDiv.appendChild(exampleSVGLabel);
-exampleSVGDiv.appendChild(exampleSVGContent);
-//add style to divs
-for (var div of [exampleInputDiv, arrow1Div, exampleNewickDiv, arrow2Div, exampleSVGDiv]){
-  div.style["margin"] = "10px";
-  exampleDiv.appendChild(div);
+exampleSubDivs[4].appendChild(exampleSVGContent);
+for (var element of [exampleInputDivInner, exampleNewickContent, exampleSVGContent]){
+  element.style["border-style"] = "solid";
+  element.style["border-color"] = "black";
+  element.style["border-width"] = "1px";
 }
 
 /*
