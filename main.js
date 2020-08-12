26f046ed2d3f1525f8dbc54dac8a222502b23540
Hannah <hannahmaryfisher@gmail.com>
2020-08-12 10:05:37 -0700

    slight changes

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 /*
 Adds elements to front page of TreeN93 site.
 Descriptions of the project and what it can be used for.
+And links to the different parts. 
 */
 
 /*
@@ -18,30 +19,34 @@ for (var element of elementsList){
 add content to title div
 */
 var titleP = document.createElement("p");
-titleP.innerHTML = "Welcome to TreeN93<br>";
-titleP.innerHTML += "TreeN93 is a website for building and visualizing single-linkage hierarchical clustering trees";
-titleP.innerHTML += " of viral sequences based on pairwise distances between the sequences and a chosen clustering threshold<br>";
-titleP.innerHTML += "View the code at <a href='https://github.com/Moshiri-Lab/TreeN93/tree/master'>https://github.com/Moshiri-Lab/TreeN93/tree/master</a><br>";
+titleP.innerHTML = "Welcome to TreeN93<br><br>";
+titleP.innerHTML += "TreeN93 is a website for building and visualizing single";
+titleP.innerHTML += "-linkage hierarchical clustering trees";
+titleP.innerHTML += " of viral sequences based on pairwise distances between"
+titleP.innerHTML += " the sequences and a user-chosen clustering threshold<br>";
+titleP.innerHTML += "View the code at"
+titleP.innerHTML += " <a target='_blank' href='https://github.com/Moshiri-Lab/TreeN93'>https://github.com/Moshiri-Lab/TreeN93</a><br>";
 titleDiv.appendChild(titleP);
 
 /*
 add content to build div
 */
 var buildP = document.createElement("p");
-buildP.innerHTML = "TreeN93 Build<br>";
+buildP.innerHTML = "TreeN93 Build<br><br>";
 buildP.innerHTML += "A tool to build a tree from pairwise TN93 distances ";
 buildP.innerHTML += "between leaf nodes, and to save the tree in the Newick file format<br>";
-buildP.innerHTML += "Use the site at <a href='https://moshiri-lab.github.io/TreeN93/Build/'>https://moshiri-lab.github.io/TreeN93/Build/</a><br>";
+buildP.innerHTML += "Use the site at <a target='_blank' href='https://moshiri-lab.github.io/TreeN93/Build/'>https://moshiri-lab.github.io/TreeN93/Build/</a><br>";
 buildDiv.appendChild(buildP);
 
 /*
 add content to viz div
 */
 var vizP = document.createElement("p");
-vizP.innerHTML = "TreeN93 Viz<br>";
-vizP.innerHTML += "A tool to visualize clusters on the tree from a user-inputted distance threshold";
-vizP.innerHTML += ", and to find the minimum threshold that yields the maximum number of clusters<br>";
-vizP.innerHTML += "Use the site at <a href='https://moshiri-lab.github.io/TreeN93/Viz/'>https://moshiri-lab.github.io/TreeN93/Viz/</a><br>";
+vizP.innerHTML = "TreeN93 Viz<br><br>";
+vizP.innerHTML += "A tool to visualize clusters on the tree from a user-"
+vizP.innerHTML += "inputted distance threshold, and to find the minimum";
+vizP.innerHTML += " threshold that yields the maximum number of clusters<br>";
+vizP.innerHTML += "Use the site at <a target='_blank' href='https://moshiri-lab.github.io/TreeN93/Viz/'>https://moshiri-lab.github.io/TreeN93/Viz/</a><br>";
 vizDiv.appendChild(vizP);
 
 /*
